de8ed9aaa8f5979e19be3c17d76e8dfe3c751d57
Carsten Pleiser <carsten@eventtechpartner.com>
2019-05-21 21:26:27 +0100

    Added show song route and backend

--- client/src/components/ViewSong.vue
+++ client/src/components/ViewSong.vue
@@ -5,8 +5,18 @@
 </template>
 
 <script>
+import SongService from '@/services/SongService'
 export default {
-
+  data () {
+    return {
+      song: null
+    }
+  },
+  async mounted () {
+    const songId = this.$store.state.route.params.songId
+    console.log(songId)
+    this.song = (await SongService.show(songId)).data
+  }
 }
 </script>
 
